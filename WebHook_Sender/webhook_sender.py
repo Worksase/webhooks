efb53879ca8246a62ccfcd38666b14ebf628d18d
SleportV3 <81850071+SleportV3@users.noreply.github.com>
2021-06-24 09:12:37 +0200

    Update webhook_sender.py

--- WebHook_Sender/webhook_sender.py
+++ WebHook_Sender/webhook_sender.py
@@ -1,12 +1,10 @@
-from discord_webhook import DiscordWebhook
+from discord_webhook import DiscordWebhook, DiscordEmbed
 import discord
 from discord.ext import commands
 import os
 import colorama
 from os import name, system, getenv
 from colorama import Fore, init
-import requests
-
 
 if name == "nt":
     system("title Webhook_sender")
@@ -23,12 +21,14 @@ mainbanner = (Fore.RED + """
                                  ██║ █╗ ██║█████╗  ██████╔╝███████║██║   ██║██║   ██║█████╔╝ 
                                  ██║███╗██║██╔══╝  ██╔══██╗██╔══██║██║   ██║██║   ██║██╔═██╗ 
                                  ╚███╔███╔╝███████╗██████╔╝██║  ██║╚██████╔╝╚██████╔╝██║  ██╗
-                                  ╚══╝╚══╝ ╚══════╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ 
+                                  ╚══╝╚══╝ ╚══════╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝
 """) 
 choix = (Fore.GREEN + """
                                  ____________________________________________________________
                                                                                                                       
-                                            1 : Envoyer un message    2 : Crédit                                                                                                                                                                                                           
+                                       1 : Envoyer un message    2 : Envoyer des embeds 
+
+                                       3: Crédit                                                                                                                                                                                                          
                                  ____________________________________________________________  
 
                                                 https://discord.gg/SaDR4jgKt6                                                            
@@ -42,27 +42,36 @@ user_answer = input('[>]Veuillez entrer votre réponse: ')
 if user_answer == "1":
 
     webhook = input(Fore.YELLOW + "[>]Veuillez entrer le lien de votre webhook: ")
-    username = input(Fore.YELLOW + "[>]Veuillez entrer le pseudo de votre webhook: ")
     message = input("[>]Veuillez entrer le message à envoyer: ")
 
-    webhook = DiscordWebhook(url=webhook, content=message, username=username)
+    webhook = DiscordWebhook(url=webhook, content=message)
     response = webhook.execute()
 
     print("Le message " + Fore.RED + message + Fore.YELLOW +  " à bien été envoyé !")
-    input("Pressez [ENTRER] pour retourner au menu")
+    input("Pressez [ENTRER] pour quitter")
 
-if user_answer == "2":
+if user_answer == "2":    
+    print(Fore.RED + "[Ne passez pas les étapes sauf le pseudo, le titre, un fix sera bientot donné pour passer la majorité de l'embed]")
+    Webhook = input(Fore.YELLOW + "[>]Veuillez entrer le lien de votre webhook:")
+    title = input("[>]Veuillez entrer le titre de l'embed: ")
+    author = input("[>]Veuillez entrer le pseudo du webhook: ")
+    description = input("[>]Veuillez entrer la description de l'embed: ")
+    fieldname = input("[>]Veuillez entrer le nom du field de l'embed: ")
+    field = input("[>]Veuillez entrer le texte du field de l'embed: ")
+    footer = input("[>]Veuillez entrer le footer de l'embed: ")
+    color = input("[>]Veuillez entrer la  couleur de l'embed: ")
+    print(Fore.RED + "L'embed à bien été envoyé !")
+    input("Pressez [ENTRER] pour quitter")
+
+
+    webhook = DiscordWebhook(url=Webhook, username=author)
+    embed = DiscordEmbed(title=title, description=description, color=color)
+    embed.set_footer(text=footer)
+    embed.add_embed_field(name=fieldname, value=field)
+
+    webhook.add_embed(embed)
+    response = webhook.execute()
+
+if user_answer == "3":
     print(Fore.YELLOW + "https://discord.gg/SaDR4jgKt6")
-
     input("Pressez [ENTRER] pour retourner au menu")
-
-
-
-
-
-
-
-
-
-
-    
